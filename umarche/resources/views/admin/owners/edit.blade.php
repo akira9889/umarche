b731c116d7033bf7cc5c4c70263c89299a1f9bfe
akira9889 <aki_badmin89@icloud.com>
2023-03-30 18:00:16 +0900

    shopCreateDelete

--- umarche/resources/views/admin/owners/edit.blade.php
+++ umarche/resources/views/admin/owners/edit.blade.php
@@ -15,7 +15,8 @@
                                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー情報登録</h1>
                             </div>
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                                <form action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}" method="post">
+                                <form action="{{ route('admin.owners.update', ['owner' => $owner->id]) }}"
+                                    method="post">
                                     @method('PUT')
                                     @csrf
                                     <div class="-m-2">
@@ -37,6 +38,16 @@
                                                     value="{{ $owner->email }}" required
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                            </div>
+                                        </div>
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="shop"
+                                                    class="leading-7 text-sm text-gray-600">店名</label>
+                                                <div
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    {{ $owner->shop->name }}
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="p-2 w-1/2 mx-auto">
