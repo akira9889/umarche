fcca86ad37632aa0ec5a03181ac3a8474aed7a6d
akira9889 <aki_badmin89@icloud.com>
2023-03-31 12:53:31 +0900

    index画面

--- umarche/app/Http/Controllers/Owner/Shopcontroller.php
+++ umarche/app/Http/Controllers/Owner/Shopcontroller.php
@@ -34,8 +34,7 @@ class Shopcontroller extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index', compact('shops'));
     }
