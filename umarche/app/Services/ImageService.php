9682e31af7e4008206da10751cbcf0b9a2698cde
akira9889 <aki_badmin89@icloud.com>
2023-04-01 20:11:55 +0900

    store

--- umarche/app/Services/ImageService.php
+++ umarche/app/Services/ImageService.php
@@ -9,10 +9,17 @@ class ImageService
 {
     public static function upload($imageFile, $folderName)
     {
+        if (is_array($imageFile)) {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
         $fileName = uniqid(rand() . '_');
-        $extension = $imageFile->extension();
+        $extension = $file->extension();
         $fileNameToStore = $fileName . '.' . $extension;
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        $width = 1920;
+        $height = 1080;
+        $resizedImage = InterventionImage::make($file)->resizeCanvas($width, $height)->encode();
         Storage::put('public/'.$folderName.'/' . $fileNameToStore, $resizedImage);
 
         return $fileNameToStore;
