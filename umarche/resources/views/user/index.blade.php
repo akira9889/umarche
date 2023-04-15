c7ec4e06bfeaa37952ba40d1dcaa5e2711adeb0b
akira9889 <aki_badmin89@icloud.com>
2023-04-15 15:51:23 +0900

    表示件数

--- umarche/resources/views/user/index.blade.php
+++ umarche/resources/views/user/index.blade.php
@@ -27,7 +27,20 @@
                                 </option>
                             </select>
                         </div>
-                        <div>表示件数</div>
+                        <div>
+                            <span>表示件数</span><br>
+                            <select name="pagination" id="pagination">
+                                <option value="20"
+                                    @if (\Request::get('pagination') === '20') selected @endif>20件
+                                </option>
+                                <option value="50"
+                                    @if (\Request::get('pagination') === '50') selected @endif>50件
+                                </option>
+                                <option value="100"
+                                    @if (\Request::get('pagination') === '100') selected @endif>100件
+                                </option>
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -58,6 +71,10 @@
                             </div>
                         @endforeach
                     </div>
+                    {{ $products->appends([
+                        'sort' => \Request::get('sort'),
+                        'pagination' => \Request::get('pagination'),
+                    ])->links() }}
                 </div>
             </div>
         </div>
@@ -66,6 +83,12 @@
         const select = document.getElementById('sort');
 
         select.addEventListener('change', e => {
+            e.target.form.submit();
+        });
+
+        const paginate = document.getElementById('pagination');
+
+        paginate.addEventListener('change', e => {
             e.target.form.submit();
         });
     </script>
